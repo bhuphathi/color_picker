e6a875ef9ea460a16d775ae97394032362330fb5
bhuphathi <bhuphathi@gmail.com>
2024-05-23 13:38:47 +0530

    fix: rgb to hex value

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const addColumnButton = document.getElementById('addColumnButton');
 const defaultColor = '#30363d';
 
 // Create HTML content as a string
-const eyeDropperIconString = '<svg xmlns="http://www.w3.org/2000/svg" height="23" viewBox="0 0 18 18" width="23"><rect id="Canvas" fill="#ff13dc" opacity="0" width="18" height="18"></rect><path class="fill" d="M11.2285,8.5185,4.116,15.631a1.2355,1.2355,0,0,1-1.7772-1.7168l.0302-.0302L9.4815,6.7715ZM14.864,1.053a1.79554,1.79554,0,0,0-1.273.5275L11.3285,3.843l-.707-.707a.5.5,0,0,0-.707,0L8.2335,4.8165a.5.5,0,0,0,0,.707l.5405.541L1.662,13.177a2.23516,2.23516,0,0,0,3.161,3.161l7.1125-7.112.541.5405a.5.5,0,0,0,.707,0L14.864,8.086a.5.5,0,0,0,.00039-.70711L14.864,7.3785l-.707-.707L16.4195,4.409a1.8,1.8,0,0,0,.00042-2.54558L16.4195,1.863l-.2825-.2825A1.796,1.796,0,0,0,14.864,1.053Z"></path></svg>';
+const eyeDropperIconString = '<svg xmlns="http://www.w3.org/2000/svg" height="23" viewBox="0 0 18 18" width="23"><rect id="Canvas" opacity="0" width="18" height="18"></rect><path class="fill" d="M11.2285,8.5185,4.116,15.631a1.2355,1.2355,0,0,1-1.7772-1.7168l.0302-.0302L9.4815,6.7715ZM14.864,1.053a1.79554,1.79554,0,0,0-1.273.5275L11.3285,3.843l-.707-.707a.5.5,0,0,0-.707,0L8.2335,4.8165a.5.5,0,0,0,0,.707l.5405.541L1.662,13.177a2.23516,2.23516,0,0,0,3.161,3.161l7.1125-7.112.541.5405a.5.5,0,0,0,.707,0L14.864,8.086a.5.5,0,0,0,.00039-.70711L14.864,7.3785l-.707-.707L16.4195,4.409a1.8,1.8,0,0,0,.00042-2.54558L16.4195,1.863l-.2825-.2825A1.796,1.796,0,0,0,14.864,1.053Z"></path></svg>';
 
 function createColumn(color) {
+  const errorDisplay = document.getElementById('error');
+
   // Create a new column element
   const newColumn = document.createElement('div');
   newColumn.classList.add('column');
@@ -21,14 +23,13 @@ function createColumn(color) {
   // Create color picker for the column
   const colorPicker = document.createElement('input');
   colorPicker.type = 'color';
-  colorPicker.value = defaultColor;
   colorPicker.classList.add('color-picker');
   newColumn.appendChild(colorPicker);
 
-  const colorValue = document.createElement('span');
-  colorValue.classList.add('color-value');
-  colorValue.textContent = defaultColor;
-  newColumn.appendChild(colorValue);
+  //create color value text
+  const colorValueElement = document.createElement('span');
+  colorValueElement.classList.add('color-value');
+  newColumn.appendChild(colorValueElement);
 
   // Create eye dropper button for the column
   const eyeDropperBtn = document.createElement('div');
@@ -39,29 +40,35 @@ function createColumn(color) {
   const newEyeDropperIcon = new DOMParser().parseFromString(eyeDropperIconString, 'text/html').querySelector('svg');
   eyeDropperBtn.appendChild(newEyeDropperIcon);
 
+  setColumnColor(newColumn, colorValueElement, colorPicker, defaultColor)
+
+  //eye dropper eventlistener
   eyeDropperBtn.addEventListener('click', async () => {
     if (!window.EyeDropper) {
-      colorDisplay.textContent = 'EyeDropper API not supported.';
+      errorDisplay.textContent = 'EyeDropper API not supported.';
       return;
     }
 
     try {
       const eyeDropper = new window.EyeDropper();
       const color = await eyeDropper.open();
-      colorDisplay.style.backgroundColor = color.sRGBHex;
-      colorDisplay.textContent = `Picked Color: ${color.sRGBHex}`;
+      const hexValue = rgbaToHex(color.sRGBHex);
+      const backgroundColorStyle = `background-color: ${hexValue} !important`;
+      setColumnColor(newColumn, colorValueElement, colorPicker, hexValue);
+      // colorValue.textContent = hexValue;
+      // newColumn.style.cssText = backgroundColorStyle;
+      console.log(hexValue);
+
     } catch (err) {
       console.error('Error using EyeDropper:', err);
-      colorDisplay.textContent = 'Failed to pick color.';
+      errorDisplay.textContent = 'Failed to pick color.';
     }
   });
 
   // Add event listener to update column background color on input change
   colorPicker.addEventListener('input', (event) => {
-    // newColumn.style.backgroundColor = event.target.value;
     console.log("Color value:", event.target.value);
-    colorValue.textContent = event.target.value;
-    newColumn.style.backgroundColor = event.target.value;
+    setColumnColor(newColumn, colorValueElement, colorPicker, event.target.value)
 
   });
 
@@ -80,3 +87,30 @@ columnContainer.appendChild(createColumn());
 addColumnButton.addEventListener('click', () => {
   columnContainer.appendChild(createColumn());
 });
+
+function setColumnColor(columnElement, colorValueElement, colorPicker, colorValue) {
+  columnElement.setAttribute('style', `background-color: ${colorValue};`);
+  colorValueElement.textContent = colorValue;
+  colorPicker.value = colorValue;
+}
+
+function rgbaToHex(rgbaString) {
+  const rgbValues = rgbaString.match(/(\d+),\s*(\d+),\s*(\d+)/);
+  console.log("rgbValues", rgbValues);
+  console.log("detectedHex", rgbListToHex(rgbValues));
+  //to fix error in browser color picker
+  let redValue = rgbValues[1] <= 251 ? parseInt(rgbValues[1]) + 1 : rgbValues[1];
+  let greenValue = rgbValues[2] <= 251 ? parseInt(rgbValues[2]) : rgbValues[2];
+  let blueValue = rgbValues[3] <= 251 ? parseInt(rgbValues[3]) + 1 : rgbValues[3];
+  const result = rgbListToHex(["", redValue, greenValue, blueValue])
+  console.log("setHex", result);
+
+  return result;
+}
+
+function rgbListToHex(rgbStringList) {
+  const red = parseInt(rgbStringList[1], 10).toString(16).padStart(2, '0');
+  const green = parseInt(rgbStringList[2], 10).toString(16).padStart(2, '0');
+  const blue = parseInt(rgbStringList[3], 10).toString(16).padStart(2, '0');
+  return `#${red}${green}${blue}`;
+}
