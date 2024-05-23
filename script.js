bdb824015ae0ea791d6444d01663820d33cd40ed
bhuphathi <bhuphathi@gmail.com>
2024-05-23 09:01:09 +0530

    feat: color value and default bg color

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const columnContainer = document.querySelector('.column-container');
 const addColumnButton = document.getElementById('addColumnButton');
 
+const defaultColor = '#30363d';
+
 function createColumn(color) {
   // Create a new column element
   const newColumn = document.createElement('div');
   newColumn.classList.add('column');
-  newColumn.style.backgroundColor = color || 'white'; // Set default white or provided color
-
-  // Create color picker for the column
-  const colorPicker = document.createElement('input');
-  colorPicker.type = 'color';
-  colorPicker.classList.add('color-picker');
-  newColumn.appendChild(colorPicker);
+  newColumn.style.backgroundColor = color || defaultColor; // Set default white or provided color
 
   // Create delete button for the column
   const deleteButton = document.createElement('button');
@@ -19,9 +15,25 @@ function createColumn(color) {
   deleteButton.classList.add('delete-button');
   newColumn.appendChild(deleteButton);
 
+  // Create color picker for the column
+  const colorPicker = document.createElement('input');
+  colorPicker.type = 'color';
+  colorPicker.value = defaultColor;
+  colorPicker.classList.add('color-picker');
+  newColumn.appendChild(colorPicker);
+
+  const colorValue = document.createElement('span');
+  colorValue.classList.add('color-value');
+  colorValue.textContent = defaultColor;
+  newColumn.appendChild(colorValue);
+
   // Add event listener to update column background color on input change
   colorPicker.addEventListener('input', (event) => {
+    // newColumn.style.backgroundColor = event.target.value;
+    console.log("Color value:", event.target.value);
+    colorValue.textContent = event.target.value;
     newColumn.style.backgroundColor = event.target.value;
+
   });
 
   // Add event listener to delete the column
